b3af300de3a5d3a754133ed6d9a57994c320ad19
Ishaan Vepakomma <ishaan.vepakomma@elev.ga.ntig.se>
2024-02-21 10:40:11 +0100

    First commit

---
+++ Slutprojekt-23/24/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Intrinsics.X86;
+using Raylib_cs;
+
+
+Raylib.InitWindow (1200, 1000, "Slutprojekt");
+Raylib.SetTargetFPS(60);
+
+
+
+
+
+
+
+
+
+while(!Raylib.WindowShouldClose())
+{
+    
+
+
+
+    Raylib.BeginDrawing();
+
+    
+
+
+
+
+    Raylib.EndDrawing();
+
+}
+
